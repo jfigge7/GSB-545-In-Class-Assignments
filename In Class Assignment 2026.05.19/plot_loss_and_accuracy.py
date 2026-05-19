54d19a9a91a77e1b8dc19135f3bafe6924440f3b
Jaren Figueiredo <jfigge7@gmail.com>
2026-05-19 15:01:49 -0700

    In Class Assignment 2026.05.19

---
+++ In Class Assignment 2026.05.19/plot_loss_and_accuracy.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+def plot_utils(history):
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['accuracy'], label='Train Acc')
+    plt.plot(history.history['val_accuracy'], label='Val Acc')
+    plt.title('MLP Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['loss'], label='Train Loss')
+    plt.plot(history.history['val_loss'], label='Val Loss')
+    plt.title('MLP Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
